203b23f1272f47114ae5fbd9f283daab91cfedb3
Starliege <urrianif@gmail.com>
2022-09-01 15:15:55 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    emailArr: [],
+  },
+  methods: {
+    request: function () {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          console.log(response.data.response);
+          this.emailArr.push(response.data.response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted: function () {
+    for (let i = 0; i < 10; i++) {
+      this.request();
+    }
+  },
+});
